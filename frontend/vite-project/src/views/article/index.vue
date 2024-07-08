9f3a10ec251d9bc165740c6805acbec3a5307849
kaki <kaki@aa.com>
2024-07-08 21:33:19 +0900

    aa

--- frontend/vite-project/src/views/article/index.vue
+++ frontend/vite-project/src/views/article/index.vue
@@ -41,7 +41,7 @@
 
 <script lang="ts" setup>
 import ArticleV1 from '@/components/article/Article-v1.vue'
-import ArticleV3 from '@/components/article/Article-v3.vue'
+// import ArticleV3 from '@/components/article/Article-v3.vue'
 import MyCardV1 from '@/components/global/my-card-v1.vue'
 import MyCardFilterV1 from '@/components/global/my-card-filter-v1.vue'
 
