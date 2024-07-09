bc72966f8a0dd63cef8e2506479072c4fd0e8bde
kaki <kaki@aa.com>
2024-07-10 01:11:22 +0900

    aa

--- frontend/vite-project/src/layouts/default/Default.vue
+++ frontend/vite-project/src/layouts/default/Default.vue
@@ -2,7 +2,7 @@
   <v-app>
     <default-system-bar @click:toggle="drawer = !drawer" />
 
-    <default-drawer v-model="drawer" />
+    <default-drawer v-model="drawer.value" />
 
     <default-view />
   </v-app>
@@ -10,12 +10,14 @@
 
 <script setup>
 // Components
-import DefaultDrawer from './Drawer.vue'
-import DefaultSystemBar from './SystemBarMy.vue'
-import DefaultView from './View.vue'
+import DefaultDrawer from './Drawer.vue';
+import DefaultSystemBar from './SystemBarMy.vue';
+import DefaultView from './View.vue';
 
 // Utilities
-import { shallowRef } from 'vue'
+import { shallowRef } from 'vue';
 
-const drawer = shallowRef<boolean | null>(null)
+const drawer = shallowRef<boolean>(false); // 正しい型指定と初期値の設定
+
 </script>
+
